83e0b802dafec8b4174aa1d30b2b83c9a8a4c86b
Kendra Egert <Kendra Egert>
2016-09-19 19:43:33 -0400

    initial and final commit

---
+++ ListHomework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating a boolean list to evaluate
+
+            List<bool> newBoolList = new List<bool>() { true, false, false, true, false };
+            // Console.WriteLine(newBoolList[0]); testing what will print out of list
+             
+            foreach (bool gonnaRain in newBoolList) //telling the loop to go thru each list value.
+               
+                    // evaluating the contents of the list and sending output to the console.
+                if (gonnaRain == true)
+                {
+                    Console.WriteLine("Better Bring an umbrella.");
+                } else
+                {
+                    Console.WriteLine("No rain today, enjoy the sun.");
+                }
+                Console.ReadLine();
+
+
+            //    
+        }
+    }  
+}
+    
